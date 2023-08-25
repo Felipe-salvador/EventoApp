d986dd185eda9e6973d62da3c34afadd1cbd3796
Felipe salvador <felipe2014salvador@gmail.com>
2023-08-25 16:39:41 -0300

    Saving form data to the database

--- src/main/java/com/eventoapp/eventoapp/controller/EventoController.java
+++ src/main/java/com/eventoapp/eventoapp/controller/EventoController.java
@@ -1,13 +1,27 @@
 package com.eventoapp.eventoapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eventoapp.eventoapp.models.Evento;
+import com.eventoapp.eventoapp.repository.EventoRepository;
 
 @Controller
 public class EventoController {
+	
+	@Autowired
+	private EventoRepository er;
 
-	@RequestMapping("/cadastraEvento")
+	@RequestMapping(value="/cadastraEvento", method=RequestMethod.GET)
 	public String form() {
 		return "evento/formEvento";
+	}
+	
+	@RequestMapping(value="/cadastraEvento", method=RequestMethod.POST)
+	public String form(Evento evento) {	
+		er.save(evento);
+		return "redirect:/cadastraEvento";
 	}
 }
--- src/main/java/com/eventoapp/eventoapp/models/Evento.java
+++ src/main/java/com/eventoapp/eventoapp/models/Evento.java
@@ -19,44 +19,35 @@ public class Evento implements Serializable{
 	private String local;
 	private String data;
 	private String horario;
-	
 	public long getCodigo() {
 		return codigo;
 	}
-	
 	public void setCodigo(long codigo) {
 		this.codigo = codigo;
 	}
-	
 	public String getNome() {
 		return nome;
 	}
-	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
 	public String getLocal() {
 		return local;
 	}
-	
 	public void setLocal(String local) {
 		this.local = local;
 	}
-	
 	public String getData() {
 		return data;
 	}
-	
-	public void setData() {
+	public void setData(String data) {
 		this.data = data;
 	}
-	
 	public String getHorario() {
 		return horario;
 	}
-	
-	public void setHorario() {
+	public void setHorario(String horario) {
 		this.horario = horario;
 	}
+	
 }
