82b0e38ac5b3008f0ee4c57a7cf2b74f4db1c533
Felipe salvador <felipe2014salvador@gmail.com>
2023-09-02 09:41:11 -0300

    improving the layout of html pages

--- src/main/java/com/eventoapp/eventoapp/controller/EventoController.java
+++ src/main/java/com/eventoapp/eventoapp/controller/EventoController.java
@@ -1,5 +1,7 @@
 package com.eventoapp.eventoapp.controller;
 
+import javax.swing.JOptionPane;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,15 @@ public class EventoController {
 	
 	@RequestMapping(value="/cadastraEvento", method=RequestMethod.POST)
 	public String form(Evento evento) {	
-		er.save(evento);
+		if(evento.getNome().isEmpty()
+				|| evento.getLocal().isEmpty()
+				|| evento.getData().isEmpty()
+				|| evento.getHorario().isEmpty()) {
+			
+		}else {
+			er.save(evento);
+		}
+		
 		return "redirect:/cadastraEvento";
 	}
 	
